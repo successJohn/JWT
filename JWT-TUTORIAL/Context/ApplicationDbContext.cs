ee4866ad24454aca6de2fc582be99f017f648a7f
Success John <successojohn@gmail.com>
2022-12-16 11:41:15 +0100

    Add project files.

---
+++ JWT-TUTORIAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using JWT_TUTORIAL.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT_TUTORIAL.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
